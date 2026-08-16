aa8e3d13830e65bed9ed3550f6b84bd2b794f2f0
BeckettWen <beckettwen@icloud.com>
2026-08-16 21:42:29 +0800

    Completion of the basic APIs Still, the whole thread pool is in design Update Tracking Number: U26Y160828-CD

--- MemoryPool.hpp
+++ MemoryPool.hpp
@@ -18,7 +18,7 @@ namespace Aegis_ThreadPool {
     // the beginning of the thread pool
     class thread_pool {
     private:
-        std::queue<std::jthread> threadPool;
+        std::deque<std::jthread> threadPool;
         bool status_isShutDown = false;
 
     public:
@@ -27,9 +27,10 @@ namespace Aegis_ThreadPool {
 
         // create a thread into the thread pool
         template<typename type ,typename... Args>
-        std::expected<void, thread_StatusCode> Insert_Into_thread_pool(type& task, Args&&... args) {
+        std::expected<std::jthread::id, thread_StatusCode> Insert_Into_thread_pool(type& task, Args&&... args) {
             try {
-                threadPool.push(std::jthread( std::forward<type>(task), std::forward<Args>(args)...));
+                threadPool.emplace_back(std::jthread( std::forward<type>(task), std::forward<Args>(args)...));
+                return threadPool.back().get_id();
             } catch (const std::exception& e) {
                 return std::unexpected<thread_StatusCode>(thread_StatusCode::Failure);
             }
@@ -43,7 +44,7 @@ namespace Aegis_ThreadPool {
             try {
                 while (!threadPool.empty()) {
                     threadPool.front().request_stop();
-                    threadPool.pop();
+                    threadPool.pop_front();
                 }
             }catch (std::exception& exception) {
                 return std::unexpected<thread_StatusCode>(Cleanup_Failure);
@@ -51,6 +52,26 @@ namespace Aegis_ThreadPool {
 
             status_isShutDown = true;
             return {};
+        }
+
+
+        std::expected<void, thread_StatusCode> execute_task(std::jthread::id& thread_id) {
+            try {
+                std::deque<std::jthread>::iterator find_result = threadPool.begin();
+                while (find_result != threadPool.end()) {
+                    if ((*find_result).get_id() == thread_id)
+                        break;
+                    else{ find_result++; }
+                }
+
+                if (find_result == threadPool.end() && (*find_result).get_id() != thread_id) {
+                    throw std::out_of_range("Task Not Been Registered");
+                }
+
+                (*find_result).join();
+            } catch (std::exception& exception) {
+                return std::unexpected<thread_StatusCode>(Failure);
+            }
         }
 
     };
