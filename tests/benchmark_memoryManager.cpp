6721aec481c0fcd4a74d20da35b494b02d0e1ec4
BeckettWen <beckettrowan@icloud.com>
2026-09-10 22:41:50 +0800

    Update: Ready to introduce the benchmark Update Tracking Number: Calcium-Sep39

---
+++ tests/benchmark_memoryManager.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Beckett on 9/9/26.
+//
+
+#include <benchmark/benchmark.h>
+
+static void Benchmark_memory_manager(benchmark::State& state){}
+
+BENCHMARK(Benchmark_memory_manager);
+
+BENCHMARK_MAIN();
