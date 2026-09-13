439347bcdab7738c90c55d703bc06a547b07d52a
BeckettWen <beckettrowan@icloud.com>
2026-09-13 20:07:05 +0800

    Update: Added the benchmark functions into the memory manager - Known issues is already under investigation Update Tracking Number: Tellurium-Sep3B

--- tests/benchmark_memoryManager.cpp
+++ tests/benchmark_memoryManager.cpp
@@ -3,11 +3,54 @@
 //
 
 #include <benchmark/benchmark.h>
+#include "../Aegis.hpp"
 
-static void Benchmark_memory_manager(benchmark::State& state) {
-
+static void BM_AllocateMemory(benchmark::State& state) {
+    for (auto _ : state) {
+        Aegis_MemoryManager::Aegis_allocator allocator;
+        benchmark::DoNotOptimize(allocator.allocateMemory(state.range(0)));
+    }
 }
+BENCHMARK(BM_AllocateMemory)->Range(1024, 1024 * 1024 * 10);
 
-BENCHMARK(Benchmark_memory_manager);
+static void BM_AllocateMemory_Optimized(benchmark::State& state) {
+    for (auto _ : state) {
+        Aegis_MemoryManager::Aegis_allocator allocator;
+        benchmark::DoNotOptimize(allocator.allocateMemory_Optimized(state.range(0)));
+    }
+}
+BENCHMARK(BM_AllocateMemory_Optimized)->Range(1024, 1024 * 1024 * 10);
+
+static void BM_GetAllocatedSize(benchmark::State& state) {
+    Aegis_MemoryManager::Aegis_allocator allocator;
+    std::size_t handle = allocator.allocateMemory(1024);
+    for (auto _ : state) {
+        auto result = allocator.getAllocatedSize(handle);
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(BM_GetAllocatedSize);
+
+static void BM_ReadData(benchmark::State& state) {
+    Aegis_MemoryManager::Aegis_allocator allocator;
+    std::size_t handle = allocator.allocateMemory(state.range(0));
+    for (auto _ : state) {
+        auto result = allocator.readData(handle);
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(BM_ReadData)->Range(1024, 1024 * 1024);
+
+static void BM_DeleteMemory(benchmark::State& state) {
+    for (auto _ : state) {
+        state.PauseTiming();
+        Aegis_MemoryManager::Aegis_allocator allocator;
+        std::size_t handle = allocator.allocateMemory(state.range(0));
+        state.ResumeTiming();
+
+        benchmark::DoNotOptimize(allocator.DeleteMemory(handle));
+    }
+}
+BENCHMARK(BM_DeleteMemory)->Range(1024, 1024 * 1024);
 
 BENCHMARK_MAIN();
